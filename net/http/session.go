de74383cdc03541648ecb11f53adc1f96d8bfdb5
leecode <leecode@qq.com>
2017-06-12 23:24:24 +0800

    add cookie & session package

---
+++ net/http/session.go
@@ -0,0 +1,17 @@
+package http
+
+// Session，需要一个map,和上次访问时间（用于Session GC)
+type Session interface {
+	Set(key, value interface{})
+	Get(key interface{}) interface{}
+	Delete(key interface{})
+	SessionId() string
+}
+
+// 维护Session列表，以及将过期的Session清除掉
+type SessionHandler interface {
+	SessionInit() Session
+	SessionDelete(sessionId string)
+	SessionGet(sessionId string) Session
+	SessionGC(maxLifeTime int64)
+}
