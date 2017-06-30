54bd554c3fa8572ccdf96041c98a8c605de28339
leecode <leecode@qq.com>
2017-07-01 02:09:57 +0800

    simply handle '/' request

--- configloader/yamlloader.go
+++ configloader/yamlloader.go
@@ -5,18 +5,18 @@ import (
 )
 
 type ConfigureLoader interface {
-	LoadConfigure(configData []byte) (map[interface{}]interface{}, error)
-	SetConfigure(key interface{}, setConfigData interface{}) error
+	LoadConfigure(configData []byte) (map[string]interface{}, error)
+	SetConfigure(key string, setConfigData interface{}) error
 }
 
 type YamlLoader struct {
 }
 
-func (yamlLoader *YamlLoader) LoadConfigure(configData []byte) (configMap map[interface{}]interface{}, err error) {
+func (yamlLoader *YamlLoader) LoadConfigure(configData []byte) (configMap map[string]interface{}, err error) {
 	err = yaml.Unmarshal(configData, &configMap)
 	return
 }
 
-func (yamlLoader *YamlLoader) SetConfigure(key interface{}, setConfigData interface{}) (err error) {
+func (yamlLoader *YamlLoader) SetConfigure(key string, setConfigData interface{}) (err error) {
 	return
 }
